6325ccdf0ae16cb41ceafd85cddd73ae27f71399
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2024-07-20 00:02:25 +0700

    add register page

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,6 +1,12 @@
+import RegisterForm from "@/components/registerForm/registerForm";
+
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="flex w-full justify-center items-center">
+      <div className="w-full h-fit xl:max-w-6xl flex justify-center items-center px-2 sm:px-5 md:px-10 lg:px-24">
+      <RegisterForm />
+      </div>
+    </div>
   )
 }
 
