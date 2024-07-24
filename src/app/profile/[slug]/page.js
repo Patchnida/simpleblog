a5a6ff3037ae4516092e71c924ce16fcdb3060d8
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2024-07-24 22:45:32 +0700

    update frontend

--- src/app/profile/[slug]/page.js
+++ src/app/profile/[slug]/page.js
@@ -1,7 +1,10 @@
+'use client'
+
 import BlogCard from "@/components/blogCard/blogCard";
 import ProfileCard from "@/components/profileCard/profileCard";
 import SearchBlog from "@/components/searchBlog/searchBlog";
 import SearchCategory from "@/components/searchCategory/searchCategory";
+import Link from 'next/link';
 
 const SingleProfilePage = () => {
   return (
@@ -10,7 +13,19 @@ const SingleProfilePage = () => {
           <div className="flex justify-center items-center">
             <ProfileCard />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+          <div className="flex flex-row justify-end items-center my-5 gap-3">
+            <SearchBlog/>
+            <Link
+            href="/createBlog"
+            className="bg-zinc-950 text-white font-medium px-5 py-2 border rounded-3xl w-18 md:w-42 flex justify-center items-center hover:shadow hover:bg-zinc-800"
+            >
+              <>
+                  <p className="md:hidden text-xs">ADD</p>
+                  <p className="hidden text-base md:inline">ADD NEW BLOG</p>
+              </>
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             <BlogCard/>
             <BlogCard/>
             <BlogCard/>
@@ -20,22 +35,6 @@ const SingleProfilePage = () => {
           </div>
         </div>
     </div>
-    
-    // <div className="flex max-h-screen h-full">
-    //   <div className="flex flex-col ">
-    //     <div className="">
-    //     <ProfileCard />
-    //     </div>
-    //     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
-    //       <BlogCard/>
-    //       <BlogCard/>
-    //       <BlogCard/>
-    //       <BlogCard/>
-    //       <BlogCard/>
-    //       <BlogCard/>
-    //     </div>
-    //     </div>
-    // </div>
   )
 }
 
