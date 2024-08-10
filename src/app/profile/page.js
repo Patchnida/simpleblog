e05ea65dddb71e5cac4957bcac613dd64a627649
Patchnida Hemwannanukul <patchnida2547@gmail.com>
2024-08-10 23:22:59 +0700

    update css

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,7 +2,6 @@
 
 import BlogCard from "@/components/blogCard/blogCard";
 import ProfileCard from "@/components/profileCard/profileCard";
-import SearchBlog from "@/components/searchBlog/searchBlog";
 
 import Link from 'next/link';
 
@@ -42,47 +41,50 @@ const ProfilePage = () => {
   );
 
   return (
-    <div className="flex min-h-screen h-full">
-        <div className="flex flex-col m-5">
+    <div className="flex min-h-screen h-full w-full">
+        <div className="flex flex-col m-5 h-full w-full">
           <div className="flex justify-center items-center">
             <ProfileCard />
           </div>
           
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="w-full h-full">
           {filteredPosts.length > 0 ? (
             <>
-              <div className="flex flex-row justify-end items-center mt-5 gap-3 w-full col-span-full">
-                <Link
-                  href="/createBlog"
-                  className="bg-zinc-950 text-white font-medium px-5 py-2 border rounded-3xl w-18 md:w-42 flex justify-center items-center hover:shadow hover:bg-zinc-800"
-                >
-                  <>
-                    <p className="md:hidden text-xs">ADD</p>
-                    <p className="hidden text-base md:inline">ADD NEW BLOG</p>
-                  </>
-                </Link>
-              </div>
-              {filteredPosts.map((post) => (
-                <Link key={post._id} href={`/blog/${post._id}`} passHref>
-                  <div className="h-full">
-                    <BlogCard post={post} />
-                  </div>
-                </Link>
-              ))}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 h-full w-full">
+                <div className="flex flex-row justify-end items-center mt-5 gap-3 w-full col-span-full">
+                  <Link
+                    href="/createBlog"
+                    className="bg-zinc-950 text-white font-medium px-5 py-2 border rounded-3xl w-18 md:w-42 flex justify-center items-center hover:shadow hover:bg-zinc-800"
+                  >
+                    <>
+                      <p className="md:hidden text-xs">ADD</p>
+                      <p className="hidden text-base md:inline">ADD NEW BLOG</p>
+                    </>
+                  </Link>
+                </div>
+                {filteredPosts.map((post) => (
+                  <Link key={post._id} href={`/blog/${post._id}`} passHref>
+                    <div className="h-full w-full">
+                      <BlogCard post={post} />
+                    </div>
+                  </Link>
+                ))}
+            </div>
+              
             </>
           ) : (
-            <div className="flex flex-col w-full justify-center items-center bg-red-500 ">
-              <p className="text-center text-lg font-medium">You do not have any post.</p>
-              <Link
-                href="/createBlog"
-                className="bg-zinc-950 text-white font-medium px-5 py-2 border rounded-3xl w-fit flex justify-center items-center hover:shadow hover:bg-zinc-800 mt-5"
-              >
-                <>
-                  <p className="md:hidden text-xs">ADD</p>
-                  <p className="hidden text-base md:inline">ADD NEW BLOG</p>
-                </>
-              </Link>
-            </div>
+            <div className="flex flex-col w-full h-full p-10 mt-5 justify-center items-center">
+                <p className="text-center w-full text-sm md:text-lg font-medium ">You do not have any post.</p>
+                  <Link
+                    href="/createBlog"
+                    className="bg-zinc-950 text-white font-medium px-5 py-2 border rounded-3xl w-fit flex justify-center items-center hover:shadow hover:bg-zinc-800 mt-5"
+                  >
+                    <>
+                      <p className="md:hidden text-xs">ADD</p>
+                      <p className="hidden text-base md:inline">ADD NEW BLOG</p>
+                    </>
+                  </Link>
+              </div>
           )}
         </div>
         </div>
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className=" bg-white w-full h-16 shadow-t-md sticky bottom-0">
-        <div className="flex gap-4 justify-end mx-5 items-center  my-0 py-2 w-auto h-full">
+        <div className="flex gap-4 justify-end mx-5 items-center my-0 py-2 w-auto h-full">
             <Link href="mailto:patchnida2547@gmail.com">
                 <Image src="/email.svg" alt="" width={30} height={30}/>
             </Link>
